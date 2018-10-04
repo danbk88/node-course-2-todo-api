0518e71941ab9df7cc584226ca4517da445fde08
USER2-80809\User <danbk88@gmail.com>
2018-10-04 11:12:19 +0300

    Add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -29,6 +30,29 @@ app.get('/todos', (req,res) => {
         res.status(400).send(e);
     });
 });
+
+// Get a specific todo - query params
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        console.log('todoID is not valid');
+        return res.status(404).send('todoID is not valid');
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            //id is not in db:
+            console.log('todo is not found');
+            return res.status(404).send('todoID is not found');
+        }
+        console.log('Todo by ID: ', todo);
+         res.send({todo});
+    }).catch((e) => {
+        console.log('error', e);
+        return res.status(400).send('todoID is not found');
+
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
